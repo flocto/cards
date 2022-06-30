8436f8bb982b85fe139f91f52cc50ad0b663cc8a
flocto <alexzhang05@gmail.com>
2022-06-29 19:26:11 -0500

    removed socket testing page

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -4,7 +4,7 @@ import { Server as ServerIO } from "socket.io";
 import { Server as NetServer } from "http";
 import { Socket } from "socket.io-client";
 
-//socket testing
+//testing api
 
 export const config = {
   api: {
@@ -13,28 +13,5 @@ export const config = {
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponseServerIO) {
-  if (!res.socket.server.io) {
-    console.log("New Socket.io server created");
-
-    const httpServer: NetServer = res.socket.server as any;
-    const io = new ServerIO(httpServer, {
-      path: "/api/hello",
-    });
-
-    io.on("connect", (socket) => {
-      console.log("New client connected");
-      socket.on("update-input", (msg) => {
-        io.emit("update-input", msg);
-      });
-      socket.on('disconnect', function(){
-        console.log('Client disconnected');
-      });
-    });
-
-    res.socket.server.io = io;
-  }
-  else{
-    console.log("Socket.io server already exists");
-  }
   res.end();
 };
